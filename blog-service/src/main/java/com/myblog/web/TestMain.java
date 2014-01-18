7820a07731b0580f3eadd1265f3f9edb4205ab71
ftutor <csfangym@gmail.com>
2014-01-18 23:15:34 +0800

    integrate jooq and project dependency

---
+++ blog-service/src/main/java/com/myblog/web/TestMain.java
@@ -0,0 +1,14 @@
+package com.myblog.web;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Blog b = null;
+	}
+
+}
--- blog-service/src/test/java/com/myblog/transfer/TimerDataOnDemand.java
+++
@@ -1,6 +0,0 @@
-package com.myblog.transfer;
-import org.springframework.roo.addon.dod.RooDataOnDemand;
-
-@RooDataOnDemand(entity = Timer.class)
-public class TimerDataOnDemand {
-}
