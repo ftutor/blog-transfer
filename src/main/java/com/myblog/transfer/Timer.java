5ca07c37034a46296856241b011bf6d5895164d4
yingming.fang <yingming.fang@operasolutions.com>
2014-01-18 13:43:04 +0800

    initial frameworks

---
+++ src/main/java/com/myblog/transfer/Timer.java
@@ -0,0 +1,14 @@
+package com.myblog.transfer;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooJpaActiveRecord
+public class Timer {
+
+    /**
+     */
+    private String username;
+}
